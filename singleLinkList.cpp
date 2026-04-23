55df07bf02bad4bbf48d354cd03d50828f7af89f
khansa-qonita <khansaqonita420@gmail.com>
2026-04-23 12:21:10 +0700

    menyelesaikan program LinkedList

--- singleLinkList.cpp
+++ singleLinkList.cpp
@@ -138,10 +138,57 @@ int main()
             case '1' :
                 mhs.addNode();
                 break;
-            
 
+            case '2' :
+                if (mhs.listEmpty())
+                {
+                    cout << "\nList kosong" << endl;
+                    break;
+                }
+
+                cout << "\nMasukkan no mahasiswa yang akan dihapus : ";
+                cin >> nim;
+
+                if (mhs.delNode(nim) == false)
+                    cout << "\nData tidak ditemukan" << endl;
+                else 
+                    cout << "\nData dengan noomor mahasiswa" << nim << " berhasil dihapus\n ";
+                break;
+
+            case '3' :
+                mhs.traverse();
+                break;
+
+            case '4' :
+            {
+                if (mhs.listEmpty())
+                {
+                    cout << "\nList kosong\n";
+                    break;
+                }
+
+                Node *previous, *current;
+                cout << "\nMasukkan no mahasiswa yang dicari : ";
+                cin >> nim;
+
+                if (mhs.search(nim, previous, current) == false)
+                    cout << "\nData tidak ditemukan\n";
+                else
+                {
+                    cout << "\nData ditemukan\n";
+                    cout << "NIM Mahasiswa : " << current->noMhs << endl;
+                }
+                break;
+            }
+
+            case '5' :
+                break;
+
+            default:
+            cout << "\nPilihan salah!\n";
         }
 
-    }
-    
+    } while (ch != '5');
+
+    return 0;   
 }
