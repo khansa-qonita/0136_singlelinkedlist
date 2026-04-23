aa4ddc0acfe8236de2efdbdc8faace34c7fb3554
khansa-qonita <khansaqonita420@gmail.com>
2026-04-23 11:17:59 +0700

    menambahkan perintah data selanjutnya

--- singleLinkList.cpp
+++ singleLinkList.cpp
@@ -34,6 +34,10 @@ public:
                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
             }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
         }
     }
 }
